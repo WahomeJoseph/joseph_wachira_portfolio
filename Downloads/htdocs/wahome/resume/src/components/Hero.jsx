1df2ab9cef2c962e94b9e0bd94059a2e600825ab
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-13 10:12:26 +0000

    Changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 m-4 md:m-8">
           <button className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
-            Download Resume
+            Resume
           </button>
           <Link to="/contact">
             <button className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
