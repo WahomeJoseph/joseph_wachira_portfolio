2b95924d575f4777e6cebf63cf3cf4036d9d42cc
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-13 09:27:32 +0000

    Changes

--- Downloads/htdocs/wahome/resume/src/components/Navbar.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Navbar.jsx
@@ -1,35 +1,54 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <nav className="flex flex-row bg-bgcolor items-center justify-center min-h-screen text-txt border-b-2 border-b-uscgold">
-
-        <div className='w-full md:block md:w-auto'>
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse">
+    <nav className="bg-bgcolor border-b-2 border-b-uscgold text-txtcolor">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3">
+          <Link to="/" className="text-2xl font-semibold">
+            My Portfolio
+          </Link>
+        </div>
+        <button 
+          onClick={toggleNavbar}
+          type="button" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-txtcolor rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gpoppy" 
+          aria-controls="navbar-default" 
+          aria-expanded={isOpen}
+        >
+          <span className="sr-only">Open main menu</span>
+          <FaBars className="w-5 h-5" />
+        </button>
+        <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-bgcolor md:flex-row md:space-x-8">
             <li>
-              <Link to="/" className="block py-2 px-3 text-txtcolor bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Home</Link>
+              <Link to="/" className="block py-2 px-3 text-txtcolor bg-gpoppy rounded md:bg-transparent md:text-gpoppy md:p-0">Home</Link>
             </li>
             <li>
-              <Link to="/about" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-gpoppy dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Me</Link>
+              <Link to="/about" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gpoppy md:p-0">About Me</Link>
             </li>
             <li>
-              <Link to="/skills" className="block py-2 px-3 ttext-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-gpoppy dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</Link>
+              <Link to="/skills" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gpoppy md:p-0">Skills</Link>
             </li>
             <li>
-              <Link to="/projects" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-gpoppy dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</Link>
+              <Link to="/projects" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gpoppy md:p-0">Projects</Link>
             </li>
             <li>
-              <Link to="/contact" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-gpoppy dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+              <Link to="/contact" className="block py-2 px-3 text-txtcolor rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gpoppy md:p-0">Contact</Link>
             </li>
           </ul>
         </div>
-
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
-
