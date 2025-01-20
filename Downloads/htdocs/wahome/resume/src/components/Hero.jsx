72b89d2c486b5bef4e6ef86e6012f6afe6eac5fe
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 05:07:57 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaAnglesDown, FaXTwitter } from "react-icons/fa6";
 
-import {motion} from 'framer-motion';
+import {delay, motion} from 'framer-motion';
 
 function Hero() {
   const [showMore, setShowMore] = useState(false);
@@ -33,12 +33,12 @@ function Hero() {
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 m-4 md:m-8">
           <a  href="https://docs.google.com/document/d/1GJP4jr36238ZO7xL97-NnOv1XgFISJxwNEU7h-Cy24k/edit?usp=drive_link">
-          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
             Resume
           </motion.button>
           </a>
           <Link to="/contact">
-            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay:2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
               Hire Me
             </motion.button>
           </Link>
