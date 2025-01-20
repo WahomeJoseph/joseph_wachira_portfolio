f869c7873a4de8a9c3d094f968e54c2a91ed13a2
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 06:33:50 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -33,12 +33,12 @@ function Hero() {
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 m-4 md:m-8">
           <a  href="https://docs.google.com/document/d/1GJP4jr36238ZO7xL97-NnOv1XgFISJxwNEU7h-Cy24k/edit?usp=drive_link">
-          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 3}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 1.2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
             Resume
           </motion.button>
           </a>
           <Link to="/contact">
-            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 3}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 1.2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
               Hire Me
             </motion.button>
           </Link>
@@ -62,7 +62,7 @@ function Hero() {
           >
             <FaGithub  />
           </a>  
-          <motion.a transition={{type: 'tween', duration: 4}} whileHover={{scale: 1.2}}
+          <motion.a transition={{type: 'tween', duration: 2}} whileHover={{scale: 1.2}}
             href="https://www.linkedin.com/in/joseph-wachira-202a7023"
             className="text-xl md:text-2xl text-white">
             <FaLinkedinIn  />
