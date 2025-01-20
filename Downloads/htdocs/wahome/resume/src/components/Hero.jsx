f5bcd3a45abd1f667e7b7bdd9b1cc3db03105a59
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 06:40:49 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -43,7 +43,7 @@ function Hero() {
             </motion.button>
           </Link>
 
-          <motion.button animate={{ y:move ? 200 : -200 }} transition={{type: 'tween', duration: 1.2}} hover whileHover={{scale: 2}} onClick={() => {setMove(!move)}} className="bg-green-700 w-40 h-12 text-white" >Hover Me</motion.button>
+          <motion.button animate={{ y:move ? 200 : -200 }} transition={{type: 'tween', duration: 1.2}} hover whileHover={{scale: 2}} drag whileDrag={{scale: 2}} onClick={() => {setMove(!move)}} className="bg-green-700 w-40 h-12 text-white" >Hover Me</motion.button>
         </div>
 
         {showMore && (
@@ -62,7 +62,7 @@ function Hero() {
           >
             <FaGithub  />
           </a>  
-          <motion.a transition={{type: 'tween', duration: 2}} whileHover={{scale: 1.2}}
+          <motion.a transition={{type: 'tween', duration: 2}} hover whileHover={{scale: 1.2}}
             href="https://www.linkedin.com/in/joseph-wachira-202a7023"
             className="text-xl md:text-2xl text-white">
             <FaLinkedinIn  />
