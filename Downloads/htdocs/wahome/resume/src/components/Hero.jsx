177ec48d18ec8e22023a93fde233590dfdc69cd1
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 04:32:41 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaAnglesDown, FaXTwitter } from "react-icons/fa6";
 
+import {motion} from 'framer-motion';
+
 function Hero() {
   const [showMore, setShowMore] = useState(false);
   const navigate = useNavigate();
@@ -33,9 +35,9 @@ function Hero() {
           </button>
           </a>
           <Link to="/contact">
-            <button className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
               Hire Me
-            </button>
+            </motion.button>
           </Link>
         </div>
 
