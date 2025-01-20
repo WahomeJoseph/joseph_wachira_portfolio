1d986a85c14493fde0143190bdce8661a45ed5eb
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 05:15:07 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -33,12 +33,12 @@ function Hero() {
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 m-4 md:m-8">
           <a  href="https://docs.google.com/document/d/1GJP4jr36238ZO7xL97-NnOv1XgFISJxwNEU7h-Cy24k/edit?usp=drive_link">
-          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+          <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 3}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
             Resume
           </motion.button>
           </a>
           <Link to="/contact">
-            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay:2}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+            <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} transition={{delay: 3}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
               Hire Me
             </motion.button>
           </Link>
@@ -56,7 +56,7 @@ function Hero() {
         </button>
 
         <ul className="flex flex-row flex-wrap items-center justify-center space-x-4 md:space-x-6 mt-6">
-          <motion.a animate={{rotate: rotate ? 360 : 0}} onClick={() => {setRotate(!rotate)}}
+          <motion.a transition={{type: 'spring', stiffness: 300}}
             href="https://github.com/WahomeJoseph"
             className="text-xl md:text-2xl text-white hover:text-uscgold transition duration-300"
           >
