079a7f4398cbfab7cc9d0fa6f3030a152bf1f12b
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 04:26:39 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaAnglesDown, FaXTwitter } from "react-icons/fa6";
 
-
 function Hero() {
   const [showMore, setShowMore] = useState(false);
   const navigate = useNavigate();
