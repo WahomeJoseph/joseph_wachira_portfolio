bab6d7ed1bfca6650b3ffa1323ef1ce9e73c221f
Joseph Wahome <122433700+WahomeJoseph@users.noreply.github.com>
2025-01-20 04:41:16 +0000

    changes

--- Downloads/htdocs/wahome/resume/src/components/Hero.jsx
+++ Downloads/htdocs/wahome/resume/src/components/Hero.jsx
@@ -31,10 +31,11 @@ function Hero() {
         </p>
 
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 m-4 md:m-8">
-          <a href="https://docs.google.com/document/d/1GJP4jr36238ZO7xL97-NnOv1XgFISJxwNEU7h-Cy24k/edit?usp=drive_link"><button className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
+          <motion.a animate={{ x: 100, scale: 1 }} initial={{scale:0}} href="https://docs.google.com/document/d/1GJP4jr36238ZO7xL97-NnOv1XgFISJxwNEU7h-Cy24k/edit?usp=drive_link">
+          <button className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
             Resume
           </button>
-          </a>
+          </motion.a>
           <Link to="/contact">
             <motion.button animate={{ x: 100, scale: 1 }} initial={{scale:0}} className="w-44 h-12 text-lg md:text-xl font-semibold px-4 py-2 rounded-md shadow-md hover:bg-hgold hover:shadow-lg transition duration-300 bg-gbrown text-bgcolor">
               Hire Me
