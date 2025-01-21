1ac4aa449b8b4eefeb19f9f99676e3068b28732a
wahomeJohn <johnweru206@gmail.com>
2025-01-21 15:25:22 +0300

    changes

--- Downloads/htdocs/wahome/resume/tailwind.config.js
+++ Downloads/htdocs/wahome/resume/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         glow: 'glow 1.5s infinite',
       },
       colors: {
-        bgcolor: '##0C0404',
+        bgcolor: '#0C0404',
         txtcolor: '#cccccc',
         gold: '#ffd700',
         hgold: '#fcc200',
