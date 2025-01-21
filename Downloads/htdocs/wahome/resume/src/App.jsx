ae1b3d19c660b32b7df4d10f1f97e0ff3c608838
wahomeJohn <johnweru206@gmail.com>
2025-01-21 13:13:42 +0300

    changes

--- Downloads/htdocs/wahome/resume/src/App.jsx
+++ Downloads/htdocs/wahome/resume/src/App.jsx
@@ -1,24 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "./components/Hero";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Skills from "./pages/Skills";
-import Contact from "./pages/Contact";
+import { AnimateRoutes } from "./components/AnimateRoutes";
+
+
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>   
       <Navbar/>   
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AnimateRoutes />
     </Router>
   );
 }
---
+++ Downloads/htdocs/wahome/resume/src/components/AnimateRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Hero from "./components/Hero";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
+
+export const AnimateRoutes = () => {
+  return (
+    <div>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+    </div>
+  )
+}
