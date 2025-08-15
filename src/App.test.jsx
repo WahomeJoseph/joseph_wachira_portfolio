f88525866555ac87676f34f38ad67cf82b0c803c
wahomeJohn <johnweru206@gmail.com>
2025-08-15 09:22:30 +0300

    Edit package file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
-import { test } from 'vitest'
-import { render } from '@testing-library/react'
-// import App from './App'
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders without crashing', () => {
   render(<App />)
+  expect(screen.getByText(/your app text/i)).toBeInTheDocument()
 })
