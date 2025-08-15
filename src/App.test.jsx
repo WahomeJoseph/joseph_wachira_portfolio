f691a9b9a10d2bfbf4b113344cc991e5a1e7f7a6
wahomeJohn <johnweru206@gmail.com>
2025-08-15 08:53:29 +0300

    Edit package file

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { test } from 'vitest'
+import { render } from '@testing-library/react'
+// import App from './App'
+import App from './App'
+
+test('renders without crashing', () => {
+  render(<App />)
+})
