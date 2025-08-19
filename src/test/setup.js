e1216a5c6069b521e3a145974d7df31eb4683283
wahomeJohn <johnweru206@gmail.com>
2025-08-19 10:59:26 +0300

    remove tests

--- src/test/setup.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from 'vitest'
-import { render } from '@testing-library/react'
-import App from '../App'
-
-test('renders without crashing', () => {
-  const { container } = render(<App />)
-  expect(container).toBeDefined()
-})
